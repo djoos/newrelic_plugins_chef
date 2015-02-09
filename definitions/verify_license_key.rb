538130874c789d6b0bdaa864c825f193dfd8e6a5
David Joos <david.joos@gmail.com>
2015-02-09 13:55:25 +0000

    Fix for incorrect New Relic license key validation

--- definitions/verify_license_key.rb
+++ definitions/verify_license_key.rb
@@ -1,5 +1,5 @@
 define :verify_license_key do
-  unless /^[0-9a-fA-F]{40}$/ =~ params[:name]
+  unless /^[0-9a-zA-Z]{40}$/ =~ params[:name]
     Chef::Application.fatal!("The provided New Relic License Key is invalid: #{params[:name]}." +
         " For more information, see https://docs.newrelic.com/docs/subscriptions/license-key")
   end
